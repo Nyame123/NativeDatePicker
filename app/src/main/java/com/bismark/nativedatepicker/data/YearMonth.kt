77ca4ea74814359dd71198250a2131027928cd5f
nyame <nyamebismark12.nb@gmail.com>
2023-11-18 22:34:13 +0000

    Creates a native datepicker in jetpack compose

---
+++ app/src/main/java/com/bismark/nativedatepicker/data/YearMonth.kt
@@ -0,0 +1,11 @@
+package com.bismark.nativedatepicker.data
+
+import androidx.compose.runtime.Stable
+
+@Stable
+data class YearMonth(
+    val year: Int,
+    val month: Int
+)
+
+infix fun Int.on(year: Int): YearMonth = YearMonth(year, this)
